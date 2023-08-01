6c5023585628222cdb8a3c8f5e41973566ebf740
ilokicodes <lokesh.loki.codes@gmail.com>
2023-08-01 15:49:28 +0530

    Redux added in sm app

--- Frontend/client/lokigram/src/App.js
+++ Frontend/client/lokigram/src/App.js
@@ -1,45 +1,60 @@
 import React from 'react';
-import Burger from './components/Burger.menu';
+import { useEffect } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import PageRender from './customRouter/PageRender';
+import PrivateRouter from './customRouter/PrivateRouter';
 
 import smlogo from './assets/smlogo.png';
 
 function App() {
+
+  const { auth, status, modal, call } = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(refreshToken())
+
+    const socket = io()
+    dispatch({type: GLOBALTYPES.SOCKET, payload: socket})
+    return () => socket.close()
+  },[dispatch])
+
+  useEffect(() => {
+    if(auth.token) {
+      dispatch(getPosts(auth.token))
+      dispatch(getSuggestions(auth.token))
+      dispatch(getNotifies(auth.token))
+    }
+  }, [dispatch, auth.token])
+
+  
+  useEffect(() => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    else if (Notification.permission === "granted") {}
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {}
+      });
+    }
+  },[])
+
+  useEffect(() => {
+    const newPeer = new Peer(undefined, {
+      path: '/', secure: true
+    })
+    
+    dispatch({ type: GLOBALTYPES.PEER, payload: newPeer })
+  },[dispatch])
   
   return (
     
     <div className="App">
       
-      <header className="App-header">
-        <span className='burger'><Burger /><i class="fa fa-bars" aria-hidden="true"></i></span>
-        <img className='' src={smlogo} alt='AppLogo' />
-        <span className='title'>Lokigram</span>
-        <nav>
-          <div className='nav'>
-            <ul className='ul'>
-              <li className=''><a href='' className='notification'><i class="fa fa-flag-o" aria-hidden="true"></i><Notifications /></a></li>
-              <li className=''><a href='' className='messages'><i class="fa fa-comments-o" aria-hidden="true"></i><Messages /></a></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-
-      <Storytab />
-
-      <main>
-
-        <div className='mainbody'>
-          <PostBody />
-        </div>
-
-        <div className=''>
-          <button type='submit' className='createpost'>Create New Post<i class="fa fa-plus-square" aria-hidden="true"></i></button>
-          <button type='submit' className='search'><i class="fa fa-search" aria-hidden="true"></i></button>
-          <button type='submit' className='home'><i class="fa fa-home" aria-hidden="true"></i></button>
-        </div>
-      </main>
+      <Signup />
       
       <footer>
-          <button type='submit' className='profile'><i class="fa fa-user-o" aria-hidden="true"><User /></i></button>
           <p className=''>Lokigram Version 0.1.0</p>
       </footer>
     
