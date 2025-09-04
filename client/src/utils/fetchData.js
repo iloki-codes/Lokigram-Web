3d1981615814067255c349669b3333a2ff36a3bc
Lokesh <lokesh.loki.codes@gmail.com>
2025-09-05 00:08:44 +0530

    cors resolved

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`${BASE_URL}/${url}`, {
+    const res = await axios.get(`https://social-media-app-lokigram.onrender.com/${url}`, {
         headers: { Authorization: token}
     });
     return res;
@@ -13,7 +13,7 @@ export const getDataAPI = async (url, token) => {
 
 export const postDataAPI = async (url, post, token) => {
     console.log("Post payload:", post);
-    const res = await axios.post(`${BASE_URL}/${url}`, post, {
+    const res = await axios.post(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
         headers: {
             'Content-Type': 'application/json',
             Authorization: token
@@ -24,21 +24,21 @@ export const postDataAPI = async (url, post, token) => {
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`${BASE_URL}/${url}`, post, {
+    const res = await axios.put(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`${BASE_URL}/${url}`, post, {
+    const res = await axios.patch(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`${BASE_URL}/${url}`, {
+    const res = await axios.delete(`https://social-media-app-lokigram.onrender.com/${url}`, {
         headers: { Authorization: token}
     })
     return res;
