7583be346320181a6e7fb24fe0e1ac8c6578ddaa
Lokesh <lokesh.loki.codes@gmail.com>
2025-09-05 00:21:35 +0530

    frontedn deployed

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`https://social-media-app-lokigram.onrender.com/${url}`, {
+    const res = await axios.get(`https://social-media-app-lokigram.onrender.com/api/v1/${url}`, {
         headers: { Authorization: token}
     });
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
+    const res = await axios.post(`https://social-media-app-lokigram.onrender.com/api/v1/${url}`, post, {
         headers: {
             'Content-Type': 'application/json',
             Authorization: token
@@ -22,21 +22,21 @@ export const postDataAPI = async (url, post, token) => {
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
+    const res = await axios.put(`https://social-media-app-lokigram.onrender.com/api/v1/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`https://social-media-app-lokigram.onrender.com/${url}`, post, {
+    const res = await axios.patch(`https://social-media-app-lokigram.onrender.com/api/v1/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`https://social-media-app-lokigram.onrender.com/${url}`, {
+    const res = await axios.delete(`https://social-media-app-lokigram.onrender.com/api/v1/${url}`, {
         headers: { Authorization: token}
     })
     return res;
