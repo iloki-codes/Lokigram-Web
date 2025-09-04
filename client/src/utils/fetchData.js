7b4c54f8d9e278a65c413f4794bd6e4ccf6e667b
Lokesh <lokesh.loki.codes@gmail.com>
2025-09-04 23:48:12 +0530

    backend depolyed

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const BASE_URL = "http://localhost:5000" || process.env.REACT_APP_BASE_URL;
+// http://localhost:5000/api/v1
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`http://localhost:5000/api/v1/${url}`, {
+    const res = await axios.get(`${BASE_URL}/${url}`, {
         headers: { Authorization: token}
     });
     return res;
@@ -11,7 +13,7 @@ export const getDataAPI = async (url, token) => {
 
 export const postDataAPI = async (url, post, token) => {
     console.log("Post payload:", post);
-    const res = await axios.post(`http://localhost:5000/api/v1/${url}`, post, {
+    const res = await axios.post(`${BASE_URL}/${url}`, post, {
         headers: {
             'Content-Type': 'application/json',
             Authorization: token
@@ -22,21 +24,21 @@ export const postDataAPI = async (url, post, token) => {
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`http://localhost:5000/api/v1/${url}`, post, {
+    const res = await axios.put(`${BASE_URL}/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`http://localhost:5000/api/v1/${url}`, post, {
+    const res = await axios.patch(`${BASE_URL}/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`http://localhost:5000/api/v1/${url}`, {
+    const res = await axios.delete(`${BASE_URL}/${url}`, {
         headers: { Authorization: token}
     })
     return res;
